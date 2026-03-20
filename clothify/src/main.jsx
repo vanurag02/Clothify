a553f1f2f1504158e06e2ddd0cc7e250b2061a42
vanurag02 <anvaidya6@gmail.com>
2026-03-20 12:23:02 +0530

    Clothify Initial Commit | Mar 20, 2026

--- clothify/src/main.jsx
+++ clothify/src/main.jsx
@@ -1,29 +1,10 @@
-// Import React core library
-import React from "react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-// Import ReactDOM to render React app into the DOM
-import ReactDOM from "react-dom/client";
-
-// Import the main App component
-import App from "./App.jsx";
-
-// Import global CSS
-import "./index.css";
-
-// Import BrowserRouter to enable routing in the app
-import { BrowserRouter } from "react-router-dom";
-
-// Import global context provider for shop data
-import ShopContextProvider from "./context/ShopContext.jsx";
-
-// Render the React app into the root HTML element
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // Enable routing across the entire application
-  <BrowserRouter>
-    {/* Provide global shop data to all components */}
-    <ShopContextProvider>
-      {/* Main application component */}
-      <App />
-    </ShopContextProvider>
-  </BrowserRouter>,
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
